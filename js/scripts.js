5399f4213a566074cf8f7d450c7153823442e4d6
Tinnkie <Tinnkie@users.noreply.github.com>
2023-01-19 01:25:02 +0100

    update script

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 // A list of 3 Pokemon objects
-let pokemonList = [{name: "Butterfree", height: 1, types:['bug', 'flying']},{name: "Beedrill", height: 2, types:['bug', 'poison']},{name: "Jigglypuff", height: 1, types:['fairy', 'normal']}]
+let pokemonList = [
+    {name: 'Butterfree', height: 3.07, types:['bug', 'flying']},
+    {name: 'Beedrill', height: 3.03, types:['bug', 'poison']},
+    {name: 'Jigglypuff', height: 1.08, types:['fairy', 'normal']}
+];
+
+// A list of Pokémon appear on the web browser, highlighting Pokémons with height above 2.00
+    for (let i=0; i<pokemonList.length; i++){
+        if (pokemonList[i].height > 2.00){
+            document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') - Wow, that\'s big! </p>');
+        } else {
+            document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') </p>');
+        }
+    }

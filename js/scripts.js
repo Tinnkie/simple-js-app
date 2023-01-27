237b14a9b2166c30edb1d01df2010fb9c2ccd39d
Tinnkie <Tinnkie@users.noreply.github.com>
2023-01-27 02:31:39 +0100

    use forEach function instead for loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,25 @@ let pokemonList = [
     {name: 'Jigglypuff', height: 1.08, types:['fairy', 'normal']}
 ];
 
-// A list of Pokémon appear on the web browser, highlighting Pokémons with height above 2.00
-    for (let i=0; i<pokemonList.length; i++){
-        if (pokemonList[i].height > 2.00){
-            document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') - Wow, that\'s big! </p>');
-        } else {
-            document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') </p>');
-        }
+// A list of Pokémon in forEach() function
+pokemonList.forEach (function(pokemon){
+    if (pokemon.height > 2.00){
+        document.write ('<p>' + pokemon.name + ' (height ' + pokemon.height +') - Wow, that\'s big! </p>');
+    } else {
+        document.write ('<p>' + pokemon.name + ' (height ' + pokemon.height +') </p>');
     }
+});
+
+
+
+
+
+// OLD CODE
+// A list of Pokémon appear on the web browser, highlighting Pokémons with height above 2.00
+    // for (let i=0; i<pokemonList.length; i++){
+    //     if (pokemonList[i].height > 2.00){
+    //         document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') - Wow, that\'s big! </p>');
+    //     } else {
+    //         document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') </p>');
+    //     }
+    // }

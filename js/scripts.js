ef6943451792ce54a5a4c1a5770fbfbfbba147e8
Tinnkie <Tinnkie@users.noreply.github.com>
2023-02-01 02:25:13 +0100

    simplify code

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ let pokemonRepository = (function(){
     let pokemonList = [
         {name: 'Butterfree', height: 3.07, types:['bug', 'flying']},
         {name: 'Beedrill', height: 3.03, types:['bug', 'poison']},
-        {name: 'Jigglypuff', height: 1.08, types:['fairy', 'normal']}
+        {name: 'Jigglypuff', height: 1.08, types:['fairy', 'normal']},
+        {name: 'Ivysaur', height: 3.03, types:['grass', 'poison']},
+        {name: 'Charmander',  height: 2, types:['fire']},
+        {name: 'Roaring Moon', height: 6.07, types:['dragon', 'dark']},
     ]
 
 
@@ -14,11 +17,27 @@ let pokemonRepository = (function(){
         return pokemonList;
     }
 
+    function addListItem (pokemon){
+      let pokemons = document.querySelector ('.pokemon-list');
+      let listItem = document.createElement ('li');
+      let button = document.createElement ('button');
+      button.innerText = pokemon.name;
+      button.classList.add ('button-class');
+      listItem.appendChild (button);
+      pokemons.appendChild (listItem);
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     };
 }) ();
+
+
+pokemonRepository.getAll().forEach (function (pokemon) {
+  pokemonRepository.addListItem (pokemon);
+});
 
 
 // UNUSED (old) CODE: A list of 3 Pokemon objects
@@ -30,16 +49,13 @@ let pokemonRepository = (function(){
 
 
 // A list of Pokémon in forEach() function
-pokemonRepository.getAll().forEach (function (pokemon) {
-    if (pokemon.height > 2.00){
-        document.write (pokemon.name + ' (height ' + pokemon.height + ') - Wow, that\'s big!' + '<br>')
-    } else {
-        document.write (pokemon.name + ' (height ' + pokemon.height + ')' + '<br>')
-    }
-});
-
-
-
+// pokemonRepository.getAll().forEach (function (pokemon) {
+//     if (pokemon.height > 2.00){
+//         document.write (pokemon.name + ' (height ' + pokemon.height + ') - Wow, that\'s big!' + '<br>')
+//     } else {
+//         document.write (pokemon.name + ' (height ' + pokemon.height + ')' + '<br>')
+//     }
+// });
 
 
 // OLD CODE

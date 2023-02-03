bd6dca3b8190cf754816d0a110c7979e92f742b9
Tinnkie <Tinnkie@users.noreply.github.com>
2023-02-03 03:13:14 +0100

    remove unused code

--- js/scripts.js
+++ js/scripts.js
@@ -40,6 +40,7 @@ let pokemonRepository = (function(){
             detailsUrl: item.url
           };
           add(pokemon);
+          console.log(pokemon);
         });
       }).catch(function (e) {
         console.error(e);
@@ -78,30 +79,3 @@ pokemonRepository.loadList().then(function() {
 });
 
 
-// UNUSED (old) CODE: A list of 3 Pokemon objects
-// let pokemonList = [
-//     {name: 'Butterfree', height: 3.07, types:['bug', 'flying']},
-//     {name: 'Beedrill', height: 3.03, types:['bug', 'poison']},
-//     {name: 'Jigglypuff', height: 1.08, types:['fairy', 'normal']}
-// ];
-
-
-// A list of Pokémon in forEach() function
-// pokemonRepository.getAll().forEach (function (pokemon) {
-//     if (pokemon.height > 2.00){
-//         document.write (pokemon.name + ' (height ' + pokemon.height + ') - Wow, that\'s big!' + '<br>')
-//     } else {
-//         document.write (pokemon.name + ' (height ' + pokemon.height + ')' + '<br>')
-//     }
-// });
-
-
-// OLD CODE
-// A list of Pokémon appear on the web browser, highlighting Pokémons with height above 2.00
-    // for (let i=0; i<pokemonList.length; i++){
-    //     if (pokemonList[i].height > 2.00){
-    //         document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') - Wow, that\'s big! </p>');
-    //     } else {
-    //         document.write('<p>' + pokemonList[i].name + ' (height ' + pokemonList[i].height +') </p>');
-    //     }
-    // }
